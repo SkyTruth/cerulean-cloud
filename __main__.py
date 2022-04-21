64746a9a466e88f4307a67466650b7b57c947068
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-21 12:56:35 +0200

    Add stack to create a bucket

--- __main__.py
+++ __main__.py
@@ -1,3 +1,13 @@
 """A Python Pulumi program"""
 
 import pulumi
+from pulumi_gcp import storage
+
+project = pulumi.get_project()
+stack = pulumi.get_stack()
+
+# Create a GCP resource (Storage Bucket)
+bucket = storage.Bucket(f'{project}-{stack}-a-bucket', labels={"pulumi": "true", "environment": stack})
+
+# Export the DNS name of the bucket
+pulumi.export('bucket_name',  bucket.url)
