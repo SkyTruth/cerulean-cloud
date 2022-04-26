97f1a8013775c49e55ca4c2b340f215e9ad487a7
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-26 13:07:10 +0200

    Format

--- stack/infra.py
+++ stack/infra.py
@@ -1,13 +1,19 @@
-import pulumi 
-from pulumi_gcp import storage
+"""example infra module"""
+import pulumi
 from pulumi_aws import s3
-
+from pulumi_gcp import storage
 
 project = pulumi.get_project()
 stack = pulumi.get_stack()
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket(f'{project}-{stack}-a-bucket', location="EU", labels={"pulumi": "true", "environment": stack})
+bucket = storage.Bucket(
+    f"{project}-{stack}-a-bucket",
+    location="EU",
+    labels={"pulumi": "true", "environment": stack},
+)
 
 # Create a AWS resource (Storage Bucket)
-s3_bucket = s3.Bucket(f'{project}-{stack}-a-bucket', tags={"Pulumi": "true", "Environment": stack})
+s3_bucket = s3.Bucket(
+    f"{project}-{stack}-a-bucket", tags={"Pulumi": "true", "Environment": stack}
+)
