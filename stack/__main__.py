240212c050db09141502e8ecd4e11011de7c11c3
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-27 16:38:55 +0200

    Use native aws

--- stack/__main__.py
+++ stack/__main__.py
@@ -6,4 +6,6 @@ import titiler_sentinel
 
 # Export the DNS name of the bucket
 pulumi.export("bucket_name", infra.bucket.url)
-pulumi.export("titiler_sentinel_url", titiler_sentinel.api.arn)
+pulumi.export(
+    "titiler_sentinel_url", titiler_sentinel.lambda_titiler_sentinel_url.function_url
+)
