4334e1b073f8b2fccc477e68db7913a31e3c5bdf
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-27 16:19:42 +0200

    Use api arn

--- stack/__main__.py
+++ stack/__main__.py
@@ -6,4 +6,4 @@ import titiler_sentinel
 
 # Export the DNS name of the bucket
 pulumi.export("bucket_name", infra.bucket.url)
-pulumi.export("titiler_sentinel_url", titiler_sentinel.apigw_lambda.name)
+pulumi.export("titiler_sentinel_url", titiler_sentinel.api.arn)
