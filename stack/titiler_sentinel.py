4afa72519aa028ccaca26fd2d5afbc169bca7a93
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-27 13:58:28 +0200

    Make package with docker instead of using ECR

--- stack/titiler_sentinel.py
+++ stack/titiler_sentinel.py
@@ -1,21 +1,38 @@
 """titiler sentinel infra module"""
+import os
+
+import docker
 import pulumi
 import pulumi_aws as aws
-import pulumi_docker as docker
-from pulumi_aws import apigateway, ecr, iam, lambda_
+from pulumi_aws import apigateway, iam, lambda_
 from utils import construct_name
 
-# Build image
-repo = ecr.Repository(construct_name("registry"))
-image_name = repo.repository_url
-registry_info = None  # use ECR credentials helper.
 
-image = docker.Image(
-    construct_name("image-titiler-sentinel"),
-    build="../cerulean_cloud/titiler_sentinel",
-    image_name=image_name,
-    registry=registry_info,
-)
+# Build image
+def create_package(code_dir: str) -> pulumi.FileArchive:
+    """Build docker image and create package."""
+    print("Creating lambda package [running in Docker]...")
+    client = docker.from_env()
+
+    print("Building docker image...")
+    client.images.build(
+        path=code_dir,
+        dockerfile="../Dockerfiles/Dockerfile.titiler",
+        tag="titiler-lambda:latest",
+        rm=True,
+    )
+
+    print("Copying package.zip ...")
+    client.containers.run(
+        image="titiler-lambda:latest",
+        command="/bin/sh -c 'cp /tmp/package.zip /local/package.zip'",
+        remove=True,
+        volumes={os.path.abspath(code_dir): {"bind": "/local/", "mode": "rw"}},
+        user=0,
+    )
+
+    return pulumi.FileArchive(os.path.join(code_dir, "package.zip"))
+
 
 # Role policy to fetch S3
 iam_for_lambda = iam.Role(
@@ -36,10 +53,12 @@ iam_for_lambda = iam.Role(
 
 # Lambda function
 lambda_titiler_sentinel = lambda_.Function(
-    name=construct_name("lambda-titiler-sentinel"),
-    image_uri=image.uri,
+    resource_name=construct_name("lambda-titiler-sentinel"),
+    code=create_package("./"),
+    runtime="python3.8",
     role=iam_for_lambda.arn,
     memory_size=1024,
+    handler="handler.handler",
     environment=lambda_.FunctionEnvironmentArgs(
         variables={
             "CPL_VSIL_CURL_ALLOWED_EXTENSIONS": ".tif,.TIF,.tiff",
