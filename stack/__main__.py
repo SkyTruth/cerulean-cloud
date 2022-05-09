e3f1ada46439d0e217f5b5b88512308dd25747c9
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-05-09 12:52:27 +0200

    Fix imports with sys path append

--- stack/__main__.py
+++ stack/__main__.py
@@ -1,9 +1,8 @@
 """A Python Pulumi program"""
 
+import infra as infra
 import pulumi
-
-import stack.infra as infra
-import stack.titiler_sentinel as titiler_sentinel
+import titiler_sentinel as titiler_sentinel
 
 # Export the DNS name of the bucket
 pulumi.export("bucket_name", infra.bucket.url)
--- test/test_stack/conftest.py
+++ test/test_stack/conftest.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
 import pulumi
+
+# add stack path to enable relative imports from stack
+sys.path.append(os.path.join(os.path.abspath("."), "stack"))
 
 
 class MyMocks(pulumi.runtime.Mocks):
