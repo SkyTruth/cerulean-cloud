71561534ad5d4320f6d46ea8fe170fb5ec4a8118
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-05-10 17:48:18 +0200

    Mock titiler URL

--- test/test_cerulean_cloud/test_titiler_client.py
+++ test/test_cerulean_cloud/test_titiler_client.py
@@ -7,7 +7,7 @@ from cerulean_cloud.titiler_client import TitilerClient
 
 @pytest.fixture
 def titiler_client():
-    return TitilerClient("https://0xshe4bmk8.execute-api.eu-central-1.amazonaws.com/")
+    return TitilerClient("https://titiler.url/")
 
 
 S1_IDS = [
@@ -16,8 +16,14 @@ S1_IDS = [
 ]
 
 
-def test_get_bounds(titiler_client):
-    b = titiler_client.get_bounds(S1_IDS[0])
+def test_get_bounds(titiler_client, httpx_mock):
+    sceneid = S1_IDS[0]
+    httpx_mock.add_response(
+        method="GET",
+        url=titiler_client.url + f"bounds?sceneid={sceneid}",
+        json={"bounds": [32.989094, 43.338009, 36.540836, 45.235191]},
+    )
+    b = titiler_client.get_bounds(sceneid)
     assert len(b) == 4
     assert b == [32.989094, 43.338009, 36.540836, 45.235191]
 
@@ -31,14 +37,39 @@ def tiles_s1_scene():
     return list(tiles)
 
 
-def test_base_tile(titiler_client, tiles_s1_scene):
-    array = titiler_client.get_base_tile(S1_IDS[0], tile=tiles_s1_scene[0])
+def test_base_tile(titiler_client, tiles_s1_scene, httpx_mock):
+    sceneid = S1_IDS[0]
+    tile = tiles_s1_scene[0]
+
+    with open("test/test_cerulean_cloud/fixtures/example_tile.png", "rb") as src:
+        img_bytes = src.read()
+    httpx_mock.add_response(
+        method="GET",
+        url=titiler_client.url
+        + f"tiles/{tile.z}/{tile.x}/{tile.y}?sceneid={sceneid}&bands=vv&format=png",
+        content=img_bytes,
+    )
+    array = titiler_client.get_base_tile(S1_IDS[0], tile=tile)
     assert array.shape == (256, 256, 4)
 
 
-def test_offset_tile(titiler_client, tiles_s1_scene):
-    maxx, miny = pixel_to_location(adjacent_tile(tiles_s1_scene[0], 1, 1), 0.5, 0.5)
-    minx, maxy = pixel_to_location(tiles_s1_scene[0], 0.5, 0.5)
+def test_offset_tile(titiler_client, tiles_s1_scene, httpx_mock):
+    sceneid = S1_IDS[0]
+    tile = tiles_s1_scene[0]
+
+    maxx, miny = pixel_to_location(adjacent_tile(tile, 1, 1), 0.5, 0.5)
+    minx, maxy = pixel_to_location(tile, 0.5, 0.5)
+
+    with open("test/test_cerulean_cloud/fixtures/example_tile.png", "rb") as src:
+        img_bytes = src.read()
+
+    httpx_mock.add_response(
+        method="GET",
+        url=titiler_client.url
+        + f"crop/{minx},{miny},{maxx},{maxy}/256x256.png?sceneid={sceneid}&bands=vv",
+        content=img_bytes,
+    )
+
     array = titiler_client.get_offset_tile(S1_IDS[0], minx, miny, maxx, maxy)
     print(minx, miny, maxx, maxy)
     assert array.shape == (256, 256, 4)
