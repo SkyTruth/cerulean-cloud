f88fdd09a18ae0a8a072578e370a24c5eadb4f99
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-05-11 11:43:16 +0200

    Add further docstring

--- cerulean_cloud/titiler_client.py
+++ cerulean_cloud/titiler_client.py
@@ -17,7 +17,17 @@ class TitilerClient:
         self.url = url
 
     def get_bounds(self, sceneid: str) -> List[float]:
-        """fetch bounds of a scene"""
+        """fetch bounds of a scene
+
+        Args:
+            sceneid (str): A valid S1 scene id
+                            i.e. S1A_IW_GRDH_1SDV_20200729T034859_20200729T034924_033664_03E6D3_93EF
+
+        Returns:
+            List[float]: A 4 item list containing bounding box
+                            coordinates of the scene.
+                            (minx, miny, maxx, maxy)
+        """
         url = urlib.urljoin(self.url, "bounds")
         url += f"?sceneid={sceneid}"
         resp = httpx.get(url)
@@ -29,12 +39,26 @@ class TitilerClient:
         tile: mercantile.Tile,
         band: str = "vv",
         img_format: str = "png",
+        scale: int = 1,
     ) -> np.ndarray:
-        """get base tile as numpy array"""
+        """get base tile as numpy array
+
+        Args:
+            sceneid (str): A valid S1 scene id
+                            i.e. S1A_IW_GRDH_1SDV_20200729T034859_20200729T034924_033664_03E6D3_93EF
+            tile (mercantile.Tile): The Tile instance to fetch
+            band (str, optional): Which bands to include in the output. Defaults to "vv".
+            img_format (str, optional): File format of the output from the tiler. Defaults to "png".
+            scale (int, optional): Proxy for tile size.  1=256x256, 2=512x512... Defaults to 1.
+
+        Returns:
+            np.ndarray: The requested tile of the scene as a numpy array.
+        """
         url = urlib.urljoin(self.url, f"tiles/{tile.z}/{tile.x}/{tile.y}")
         url += f"?sceneid={sceneid}"
         url += f"&bands={band}"
         url += f"&format={img_format}"
+        url += f"&scale={scale}"
         resp = httpx.get(url)
 
         img = Image.open(BytesIO(resp.content))
@@ -53,7 +77,23 @@ class TitilerClient:
         band: str = "vv",
         img_format: str = "png",
     ) -> np.ndarray:
-        """get offset tile as numpy array"""
+        """get offset tile as numpy array (with bounds)
+
+        Args:
+            sceneid (str): A valid S1 scene id
+                            i.e. S1A_IW_GRDH_1SDV_20200729T034859_20200729T034924_033664_03E6D3_93EF
+            minx (float): Bounding box of the image.
+            miny (float): Bounding box of the image.
+            maxx (float): Bounding box of the image.
+            maxy (float): Bounding box of the image.
+            width (int, optional): Output image width. Defaults to 256.
+            height (int, optional): Output image height. Defaults to 256.
+            band (str, optional): Which bands to include in the output. Defaults to "vv".
+            img_format (str, optional): File format of the output. Defaults to "png".
+
+        Returns:
+            np.ndarray: The requested image of the bounds of the scene as a numpy array.
+        """
         url = urlib.urljoin(
             self.url, f"crop/{minx},{miny},{maxx},{maxy}/{width}x{height}.{img_format}"
         )
