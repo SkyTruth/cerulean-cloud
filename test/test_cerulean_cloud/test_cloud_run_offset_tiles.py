dd32d6bd5224243a0bc650c3a77dd41cdc2edcbe
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-05-23 10:22:14 +0200

    Use tile 20

--- test/test_cerulean_cloud/test_cloud_run_offset_tiles.py
+++ test/test_cerulean_cloud/test_cloud_run_offset_tiles.py
@@ -1,5 +1,6 @@
 import pytest
 
+from cerulean_cloud.tiling import TMS
 from cerulean_cloud.titiler_client import TitilerClient
 
 
@@ -7,5 +8,11 @@ from cerulean_cloud.titiler_client import TitilerClient
 def test_create_fixture_tile(
     url="https://0xshe4bmk8.execute-api.eu-central-1.amazonaws.com/",
 ):
-    ti = TitilerClient(url=url)
-    print(ti)
+    titiler_client = TitilerClient(url=url)
+    S1_ID = "S1A_IW_GRDH_1SDV_20200729T034859_20200729T034924_033664_03E6D3_93EF"
+
+    tiles = list(TMS.tiles(*titiler_client.get_bounds(S1_ID), [10], truncate=False))
+    tile = tiles[20]
+    # import pdb; pdb.set_trace()
+    array = titiler_client.get_base_tile(S1_ID, tile=tile, scale=2)
+    print(array)
