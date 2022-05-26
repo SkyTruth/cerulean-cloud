07ccb011785431295d53cebe1bc5b5704f8ca969
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-05-26 12:04:47 +0200

    Revert "Remove cloud run image module"
    This reverts commit d5a5fc5d75c3f6685d355fec073ab3633efc07e2.

--- stack/cloud_run_offset_tile.py
+++ stack/cloud_run_offset_tile.py
@@ -1,41 +1,10 @@
 """infra for cloud run function to perform inference on offset tiles
 Reference doc: https://www.pulumi.com/blog/build-publish-containers-iac/
 """
+import cloud_run_images
 import pulumi
-import pulumi_docker as docker
 import pulumi_gcp as gcp
-from utils import construct_name, get_file_from_gcs
-
-config = pulumi.Config()
-
-weigths_bucket = config.require("weigths_bucket")
-weigths_name = config.require("weigths_name")
-
-
-registry = gcp.container.Registry(construct_name("registry"), location="EU")
-registry_url = registry.id.apply(
-    lambda _: gcp.container.get_registry_repository().repository_url
-)
-cloud_run_offset_tile_image_url = registry_url.apply(
-    lambda url: f"{url}/cloud-run-offset-tile-image"
-)
-registry_info = None  # use gcloud for authentication.
-
-model_weights = get_file_from_gcs(
-    weigths_bucket,
-    weigths_name,
-    out_path="../cerulean_cloud/cloud_run_offset_tiles/model/model.pt",
-)
-cloud_run_offset_tile_image = docker.Image(
-    construct_name("cloud-run-offset-tile-image"),
-    build=docker.DockerBuild(
-        context="../",
-        dockerfile="../Dockerfiles/Dockerfile.cloud_run_offset",
-        extra_options=["--no-cache"],
-    ),
-    image_name=cloud_run_offset_tile_image_url,
-    registry=registry_info,
-)
+from utils import construct_name
 
 default = gcp.cloudrun.Service(
     construct_name("cloud-run-offset-tiles"),
@@ -44,7 +13,7 @@ default = gcp.cloudrun.Service(
         spec=gcp.cloudrun.ServiceTemplateSpecArgs(
             containers=[
                 gcp.cloudrun.ServiceTemplateSpecContainerArgs(
-                    image=cloud_run_offset_tile_image.base_image_name,
+                    image=cloud_run_images.cloud_run_offset_tile_image.base_image_name,
                     envs=[
                         gcp.cloudrun.ServiceTemplateSpecContainerEnvArgs(
                             name="SOURCE",
@@ -68,7 +37,9 @@ default = gcp.cloudrun.Service(
         )
     ],
     autogenerate_revision_name=True,
-    opts=pulumi.ResourceOptions(depends_on=[cloud_run_offset_tile_image]),
+    opts=pulumi.ResourceOptions(
+        depends_on=[cloud_run_images.cloud_run_offset_tile_image]
+    ),
 )
 noauth_iam_policy_data = gcp.organizations.get_iam_policy(
     bindings=[
