50fb1f37dfd663fb3c4e17ea86d22143a310c9ce
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-06-30 11:43:48 +0200

    Add postgis

--- alembic/versions/54c42e9e879f_add_postgis.py
+++ alembic/versions/54c42e9e879f_add_postgis.py
@@ -1,12 +1,10 @@
-"""First revision
+"""Add postgis
 
 Revision ID: 54c42e9e879f
 Revises:
 Create Date: 2022-06-27 16:34:29.607839
 
 """
-import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -18,12 +16,13 @@ depends_on = None
 
 def upgrade() -> None:
     """create test table"""
-    op.create_table(
-        "test",
-        sa.Column("id", sa.Integer, primary_key=True),
+    op.execute("CREATE EXTENSION postgis;")
+    op.execute("CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;")
+    op.execute(
+        "COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';"
     )
 
 
 def downgrade() -> None:
     """drop test table"""
-    op.drop_table("test")
+    op.execute("DROP EXTENSION postgis;")
