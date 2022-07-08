6e94e3522abe1a8957fc34e845f2a4dbdc303183
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-08 18:50:46 +0200

    Override app to allow to not validate DSN

--- cerulean_cloud/cloud_run_tifeatures/handler.py
+++ cerulean_cloud/cloud_run_tifeatures/handler.py
@@ -4,9 +4,17 @@ import logging
 from typing import Optional
 
 import pydantic
+from fastapi import FastAPI
 from mangum import Mangum
-from tifeatures.db import connect_to_db, register_table_catalog
-from tifeatures.main import app
+from starlette.middleware.cors import CORSMiddleware
+from starlette_cramjam.middleware import CompressionMiddleware
+from tifeatures import __version__ as tifeatures_version
+from tifeatures.db import close_db_connection, connect_to_db, register_table_catalog
+from tifeatures.errors import DEFAULT_STATUS_CODES, add_exception_handlers
+from tifeatures.factory import Endpoints
+from tifeatures.layer import FunctionRegistry
+from tifeatures.middleware import CacheControlMiddleware
+from tifeatures.settings import APISettings
 
 
 class PostgresSettings(pydantic.BaseSettings):
@@ -38,11 +46,55 @@ class PostgresSettings(pydantic.BaseSettings):
         env_file = ".env"
 
 
+settings = APISettings()
+
+app = FastAPI(
+    title=settings.name,
+    version=tifeatures_version,
+    openapi_url="/api",
+    docs_url="/api.html",
+)
+
+# Register endpoints.
+endpoints = Endpoints(title=settings.name)
+app.include_router(endpoints.router)
+
+# We add the function registry to the application state
+app.state.tifeatures_function_catalog = FunctionRegistry()
+
+# Set all CORS enabled origins
+if settings.cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.cors_origins,
+        allow_credentials=True,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
+
+app.add_middleware(CacheControlMiddleware, cachecontrol=settings.cachecontrol)
+app.add_middleware(CompressionMiddleware)
+add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+
 @app.on_event("startup")
 async def startup_event() -> None:
     """Connect to database on startup."""
+    print("using new connection")
     await connect_to_db(app, settings=PostgresSettings())
     await register_table_catalog(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event() -> None:
+    """Close database connection."""
+    await close_db_connection(app)
+
+
+@app.get("/healthz", description="Health Check", tags=["Health Check"])
+def ping():
+    """Health check."""
+    return {"ping": "pong!"}
 
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
