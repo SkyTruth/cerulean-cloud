8baa62705be822127f6a346b003f874ef98ce3e8
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-08 18:27:42 +0200

    Do not validate the DSN since cloud sql is funky https://github.com/samuelcolvin/pydantic/issues/3900

--- cerulean_cloud/cloud_run_tifeatures/handler.py
+++ cerulean_cloud/cloud_run_tifeatures/handler.py
@@ -1,9 +1,49 @@
 """Cloud run main for tifeatures
 """
 import logging
+from typing import Optional
 
+import pydantic
 from mangum import Mangum
+from tifeatures.db import connect_to_db, register_table_catalog
 from tifeatures.main import app
+
+
+class PostgresSettings(pydantic.BaseSettings):
+    """Postgres-specific API settings.
+    Attributes:
+        postgres_user: postgres username.
+        postgres_pass: postgres password.
+        postgres_host: hostname for the connection.
+        postgres_port: database port.
+        postgres_dbname: database name.
+    """
+
+    postgres_user: Optional[str]
+    postgres_pass: Optional[str]
+    postgres_host: Optional[str]
+    postgres_port: Optional[str]
+    postgres_dbname: Optional[str]
+
+    database_url: Optional[str] = None
+
+    db_min_conn_size: int = 1
+    db_max_conn_size: int = 10
+    db_max_queries: int = 50000
+    db_max_inactive_conn_lifetime: float = 300
+
+    class Config:
+        """model config"""
+
+        env_file = ".env"
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    """Connect to database on startup."""
+    await connect_to_db(app, settings=PostgresSettings())
+    await register_table_catalog(app)
+
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
