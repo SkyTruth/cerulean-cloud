f5d87cade48ed675f19b15d664cf29609d98d5c2
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-11 16:18:58 +0200

    Remove superfolous buckets

--- stack/__main__.py
+++ stack/__main__.py
@@ -4,12 +4,10 @@ import cloud_run_offset_tile
 import cloud_run_orchestrator
 import cloud_run_tifeatures
 import database
-import infra as infra
 import pulumi
 import titiler_sentinel as titiler_sentinel
 
 # Export the DNS name of the bucket
-pulumi.export("bucket_name", infra.bucket.url)
 pulumi.export("titiler_sentinel_url", titiler_sentinel.lambda_api.api_endpoint)
 pulumi.export(
     "cloud_run_offset_tile_url", cloud_run_offset_tile.default.statuses[0].url
