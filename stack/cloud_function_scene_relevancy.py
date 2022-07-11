b4c95fe39569af4a3b0b957028ab210c612e21fe
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-11 16:26:33 +0200

    Add stubs for cloud function

---
+++ stack/cloud_function_scene_relevancy.py
@@ -0,0 +1,56 @@
+"""cloud function to select appropriate scenes (over water and IW) from SNS notification"""
+import os
+import time
+
+import pulumi
+from pulumi_gcp import cloudfunctions, storage
+from utils import construct_name
+
+# We will store the source code to the Cloud Function in a Google Cloud Storage bucket.
+bucket = storage.Bucket(
+    construct_name("bucket-cloud-function-scene-relevancy"),
+    location="EU",
+    labels={"pulumi": "true", "environment": pulumi.get_stack()},
+)
+
+config_values = {"DESTINATION": "abc"}
+
+# The Cloud Function source code itself needs to be zipped up into an
+# archive, which we create using the pulumi.AssetArchive primitive.
+PATH_TO_SOURCE_CODE = "cerulean_cloud/cloud_function_scene_relevancy"
+assets = {}
+for file in os.listdir(PATH_TO_SOURCE_CODE):
+    location = os.path.join(PATH_TO_SOURCE_CODE, file)
+    asset = pulumi.FileAsset(path=location)
+    assets[file] = asset
+
+archive = pulumi.AssetArchive(assets=assets)
+
+# Create the single Cloud Storage object, which contains all of the function's
+# source code. ("main.py" and "requirements.txt".)
+source_archive_object = storage.BucketObject(
+    construct_name("source-cloud-function-scene-relevancy"),
+    name="main.py-%f" % time.time(),
+    bucket=bucket.name,
+    source=archive,
+)
+
+fxn = cloudfunctions.Function(
+    construct_name("cloud-function-scene-relevancy"),
+    entry_point="main",
+    environment_variables=config_values,
+    region=pulumi.Config("gcp").require("region"),
+    runtime="python38",
+    source_archive_bucket=bucket.name,
+    source_archive_object=source_archive_object.name,
+    trigger_http=True,
+)
+
+invoker = cloudfunctions.FunctionIamMember(
+    "invoker",
+    project=fxn.project,
+    region=fxn.region,
+    cloud_function=fxn.name,
+    role="roles/cloudfunctions.invoker",
+    member="allUsers",
+)
