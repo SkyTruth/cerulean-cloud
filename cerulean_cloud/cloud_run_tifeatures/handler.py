45bef44d0966ef0973c5852d80b6aa322ac1f227
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-11 14:24:48 +0200

    Add cerulean logo

--- cerulean_cloud/cloud_run_tifeatures/handler.py
+++ cerulean_cloud/cloud_run_tifeatures/handler.py
@@ -1,12 +1,23 @@
 """Cloud run main for tifeatures
+
+Run this locally with:
+uvicorn --port $PORT --host 0.0.0.0 cerulean_cloud.cloud_run_tifeatures.handler:app
+
+Make sure to set in your environment:
+- TIFEATURES_NAME
+- TIFEATURES_TEMPLATES
+- DATABASE_URL
+
 """
 import logging
-from typing import Optional
+from typing import Any, List, Optional
 
+import jinja2
 import pydantic
 from fastapi import FastAPI
 from mangum import Mangum
 from starlette.middleware.cors import CORSMiddleware
+from starlette.templating import Jinja2Templates
 from starlette_cramjam.middleware import CompressionMiddleware
 from tifeatures import __version__ as tifeatures_version
 from tifeatures.db import close_db_connection, connect_to_db, register_table_catalog
@@ -55,8 +66,20 @@ app = FastAPI(
     docs_url="/api.html",
 )
 
+# custom template directory
+templates_location: List[Any] = [
+    jinja2.FileSystemLoader("cerulean_cloud/cloud_run_tifeatures/templates/")
+]
+# default template directory
+# templates_location.append(jinja2.PackageLoader(__package__, "templates"))
+
+templates = Jinja2Templates(
+    directory="",  # we need to set a dummy directory variable, see https://github.com/encode/starlette/issues/1214
+    loader=jinja2.ChoiceLoader(templates_location),
+)
+
 # Register endpoints.
-endpoints = Endpoints(title=settings.name)
+endpoints = Endpoints(title=settings.name, templates=templates)
 app.include_router(endpoints.router)
 
 # We add the function registry to the application state
