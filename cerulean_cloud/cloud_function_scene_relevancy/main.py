69b63e10991458f8481d3a52c8c77756140d7622
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-12 19:13:32 +0200

    Add code to fetch a row with asyncpg

--- cerulean_cloud/cloud_function_scene_relevancy/main.py
+++ cerulean_cloud/cloud_function_scene_relevancy/main.py
@@ -1,6 +1,35 @@
 """cloud function scene relevancy handler"""
 
+import asyncio
+import os
+
+import asyncpg
+
+
+async def get_row():
+    """get a row"""
+    conn = await asyncpg.connect(os.getenv("DB_URL"))
+    row = await conn.fetchrow("SELECT * FROM trigger")
+    return row
+
 
 def main(request):
-    """test"""
-    return {"ping": "pong"}
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    row = loop.run_until_complete(get_row())
+    print(row)
+    request_json = request.get_json()
+    if request.args and "message" in request.args:
+        return request.args.get("message")
+    elif request_json and "message" in request_json:
+        return request_json["message"]
+    else:
+        return "Hello World!"
