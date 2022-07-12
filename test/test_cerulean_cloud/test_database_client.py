b8d6748ed6ce4e7b756a41182d173be1ce78d7c1
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-12 11:10:09 +0200

    Add minimal test for sentinel1 grd

--- test/test_cerulean_cloud/test_database_client.py
+++ test/test_cerulean_cloud/test_database_client.py
@@ -1,11 +1,14 @@
 """Test database client"""
+import json
+
 import pytest
 import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session, sessionmaker
 
 import cerulean_cloud.database_schema as database_schema
-from cerulean_cloud.database_client import get_engine
+from cerulean_cloud.database_client import DatabaseClient, get_engine
+from cerulean_cloud.titiler_client import TitilerClient
 
 
 def test_get_engine(postgresql):
@@ -52,3 +55,18 @@ async def test_create_model(db_session):
         model = model.scalars().first()
 
         assert model.name == "Jane Doe"
+
+
+@pytest.mark.asyncio
+async def test_create_s1l(setup_database, engine):
+    titiler_client = TitilerClient("some_url")
+    async with DatabaseClient(engine) as db_client:
+        async with db_client.session.begin():
+            with open("test/test_cerulean_cloud/fixtures/productInfo.json") as src:
+                info = json.load(src)
+            sentinel1_grd = await db_client.get_sentinel1_grd(
+                info["id"],
+                info,
+                titiler_client.get_base_tile_url(info["id"], rescale=(0, 100)),
+            )
+    assert sentinel1_grd.sceneid == info["id"]
