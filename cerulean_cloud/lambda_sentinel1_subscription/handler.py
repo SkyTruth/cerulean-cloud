0c33721dc1a2b52c477ea4abb123f705b1c66ad6
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-14 12:58:53 +0200

    print event

--- cerulean_cloud/lambda_sentinel1_subscription/handler.py
+++ cerulean_cloud/lambda_sentinel1_subscription/handler.py
@@ -7,5 +7,6 @@ import requests
 def lambda_handler(event, context):
     """handle lambda"""
     function_url = os.getenv("FUNCTION_URL")
+    print(event)
     res = requests.post(function_url, json=event)
     return {"statusCode": res.status_code}
