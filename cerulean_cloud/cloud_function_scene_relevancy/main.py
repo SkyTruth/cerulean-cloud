4cafd9cf8108e3986143b5ecf9d3fa61d510c42e
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-14 17:08:53 +0200

    Simplify main.py

--- cerulean_cloud/cloud_function_scene_relevancy/main.py
+++ cerulean_cloud/cloud_function_scene_relevancy/main.py
@@ -24,10 +24,20 @@ def load_ocean_poly():
 ocean_poly = load_ocean_poly()
 
 
-async def get_row():
+async def add_trigger_row(n_scenes=1, n_filtered_scenes=1):
     """get a row"""
     conn = await asyncpg.connect(os.getenv("DB_URL"))
     row = await conn.fetchrow("SELECT * FROM trigger")
+
+    row = await conn.fetchrow(
+        """
+        INSERT INTO trigger(scene_count, filtered_scene_count, trigger_logs, trigger_type) VALUES($1, $2, $3, $4) RETURNING id
+    """,
+        n_scenes,
+        n_filtered_scenes,
+        "",
+        "SNS_TOPIC",
+    )
     return row
 
 
@@ -40,26 +50,25 @@ def main(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+
+    request_json = request.get_json()
+    print(request_json)
+
+    filtered_scene_count = handle_notification(request_json)
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    row = loop.run_until_complete(get_row())
+    row = loop.run_until_complete(add_trigger_row(1, filtered_scene_count))
     print(row)
-    response = handler_queue()
-    print(response)
-    request_json = request.get_json()
-    if request.args and "message" in request.args:
-        return request.args.get("message")
-    elif request_json and "message" in request_json:
-        return request_json["message"]
-    else:
-        return "Hello World!"
+
+    return "Success!"
 
 
-def lambda_handler(event, context):
-    """handle lambda"""
-    print("shapely imported!")
-    if event.get("Records"):
-        sns = event["Records"][0]["Sns"]
+def handle_notification(request_json):
+    """handle notification"""
+    filtered_scene_count = 0
+    if request_json.get("Records"):
+        sns = request_json["Records"][0]["Sns"]
         msg = json.loads(sns["Message"])
         scene_poly = sh.polygon.Polygon(msg["footprint"]["coordinates"][0][0])
 
@@ -69,49 +78,10 @@ def lambda_handler(event, context):
         )  # we don't want to process any polarization other than vv XXX This is hardcoded in the server, where we look for a vv.grd file
         is_oceanic = scene_poly.intersects(ocean_poly)
         print(is_highdef, is_vv, is_oceanic)
-        """
         if is_highdef and is_vv and is_oceanic:
-            client = boto3.client("sqs", region_name="eu-central-1")
-            response = client.send_message(
-                QueueUrl="https://sqs.eu-central-1.amazonaws.com/162277344632/New_Machinable",
-                MessageBody=json.dumps(event),
-            )
-
-        sns_db_row(sns['MessageId'], sns['Subject'], sns['Timestamp'], msg, is_oceanic)
-        TODO: insert SNS into DB
-        https://docs.aws.amazon.com/lambda/latest/dg/services-rds-tutorial.html
-        """
-
-        return {"statusCode": 200}
-    else:
-        return {"statusCode": 400}
-
-
-def sns_db_row(sns_id, sns_sub, sns_ts, msg, is_oceanic):
-    # Warning! PostgreSQL hates capital letters, so the keys are different between the SNS and the DB
-    """Creates a dictionary that aligns with our SNS DB columns
-
-    Returns:
-        dict -- key for each column in our SNS DB, value from this SNS's content
-    """
-    tbl = "sns"
-    row = {
-        "sns_messageid": f"'{sns_id}'",  # Primary Key
-        "sns_subject": f"'{sns_sub}'",
-        "sns_timestamp": f"'{sns_ts}'",
-        "grd_id": f"'{msg['id']}'",
-        "grd_uuid": f"'{msg['sciHubId']}'",  # Unique Constraint
-        "absoluteorbitnumber": f"{msg['absoluteOrbitNumber']}",
-        "footprint": f"ST_GeomFromGeoJSON('{json.dumps(msg['footprint'])}')",
-        "mode": f"'{msg['mode']}'",
-        "polarization": f"'{msg['polarization']}'",
-        "s3ingestion": f"'{msg['s3Ingestion']}'",
-        "scihubingestion": f"'{msg['sciHubIngestion']}'",
-        "starttime": f"'{msg['startTime']}'",
-        "stoptime": f"'{msg['stopTime']}'",
-        "isoceanic": f"{is_oceanic}",
-    }
-    return (row, tbl)
+            # TODO add to queue
+            pass
+    return filtered_scene_count
 
 
 def handler_queue():
