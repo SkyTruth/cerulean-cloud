0ff2e70452a948e10bfdb2ac07f69bc7e4799c25
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-07-25 18:26:56 +0200

    Use json.dump

--- cerulean_cloud/cloud_function_scene_relevancy/main.py
+++ cerulean_cloud/cloud_function_scene_relevancy/main.py
@@ -151,6 +151,12 @@ def handler_queue(filtered_scenes, trigger_id):
         print(payload)
         # Add the payload to the request.
         if payload is not None:
+            if isinstance(payload, dict):
+                # Convert dict to JSON string
+                payload = json.dumps(payload)
+                # specify http content-type to application/json
+                task["http_request"]["headers"] = {"Content-type": "application/json"}
+
             # The API expects a payload of type bytes.
             converted_payload = payload.encode()
 
