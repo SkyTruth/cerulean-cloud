cc7b08fb8791578c0c15977e8dda99b491e5a8ab
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-08-01 12:10:52 +0200

    Add stubbed out stack for cloud function

--- stack/__main__.py
+++ stack/__main__.py
@@ -1,5 +1,6 @@
 """A Python Pulumi program"""
 
+import cloud_function_historical_run
 import cloud_function_scene_relevancy
 import cloud_run_offset_tile
 import cloud_run_orchestrator
@@ -21,5 +22,8 @@ pulumi.export("cloud_run_tifeatures_url", cloud_run_tifeatures.default.statuses[
 pulumi.export("database_url", database.sql_instance_url)
 pulumi.export("database_instance_name", database.instance.connection_name)
 pulumi.export("database_url_alembic", database.sql_instance_url_alembic)
-pulumi.export("fxn_url", cloud_function_scene_relevancy.fxn.https_trigger_url)
+pulumi.export(
+    "scene_relevancy_url", cloud_function_scene_relevancy.fxn.https_trigger_url
+)
+pulumi.export("historical_run_url", cloud_function_historical_run.fxn.https_trigger_url)
 pulumi.export("sns_topic_subscription", sns_subscription.sentinel1_sqs_target.arn)
