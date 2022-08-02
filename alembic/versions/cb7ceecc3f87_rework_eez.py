e1690cc68a57f788c15aa1bdf812699d5e1e8fde
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-08-02 13:18:16 +0200

    Fix views also in migration

--- alembic/versions/cb7ceecc3f87_rework_eez.py
+++ alembic/versions/cb7ceecc3f87_rework_eez.py
@@ -6,7 +6,7 @@ Create Date: 2022-08-01 16:18:55.163046
 
 """
 import sqlalchemy as sa
-from alembic_utils.pg_function import PGFunction
+from alembic_utils.pg_function import PGFunction, PGView
 from geoalchemy2 import Geography
 from sqlalchemy.types import ARRAY
 
@@ -57,6 +57,35 @@ def upgrade() -> None:
 
     op.drop_table("slick_to_eez")
 
+    slick_with_eez_and_source = PGView(
+        schema="public",
+        signature="slick_with_eez_and_source",
+        definition="// not needed",
+    )
+
+    op.drop_entity(slick_with_eez_and_source)
+
+    slick_with_source = PGView(
+        schema="public",
+        signature="slick_with_source",
+        definition="""
+    SELECT slick.*, slick_source.slick_source, slick_source.slick_source_human_confidence FROM slick
+    LEFT JOIN (
+        SELECT slick_to_slick_source_slick_source.slick,
+                array_agg(slick_to_slick_source_slick_source.name) AS slick_source,
+                array_agg(slick_to_slick_source_slick_source.human_confidence) AS slick_source_human_confidence
+        FROM (
+            SELECT slick_to_slick_source.slick, slick_source.name, slick_to_slick_source.human_confidence
+            FROM slick_to_slick_source
+            INNER JOIN slick_source
+            ON slick_source.id = slick_to_slick_source.slick_source
+        ) AS slick_to_slick_source_slick_source
+        GROUP BY slick_to_slick_source_slick_source.slick) AS slick_source
+    ON slick_source.slick = slick.id;
+    """,
+    )
+    op.create_entity(slick_with_source)
+
 
 def downgrade() -> None:
     """rework eez"""
@@ -76,3 +105,41 @@ def downgrade() -> None:
         sa.Column("eez", sa.BigInteger, sa.ForeignKey("eez.id"), nullable=False),
     )
     op.drop_column("slick", "eezs")
+
+    slick_with_eez_and_source = PGView(
+        schema="public",
+        signature="slick_with_eez_and_source",
+        definition="""
+    SELECT slick.*, eez.eez, slick_source.slick_source, slick_source.slick_source_human_confidence FROM slick
+    LEFT JOIN (
+        SELECT slick_to_eez_eez.slick, array_agg(slick_to_eez_eez.geoname) AS eez
+        FROM (
+            SELECT slick_to_eez.slick, eez.geoname
+            FROM slick_to_eez
+            INNER JOIN eez
+            ON eez.id = slick_to_eez.eez
+        ) AS slick_to_eez_eez
+        GROUP BY slick_to_eez_eez.slick) AS eez
+    ON eez.slick = slick.id
+    LEFT JOIN (
+        SELECT slick_to_slick_source_slick_source.slick,
+                array_agg(slick_to_slick_source_slick_source.name) AS slick_source,
+                array_agg(slick_to_slick_source_slick_source.human_confidence) AS slick_source_human_confidence
+        FROM (
+            SELECT slick_to_slick_source.slick, slick_source.name, slick_to_slick_source.human_confidence
+            FROM slick_to_slick_source
+            INNER JOIN slick_source
+            ON slick_source.id = slick_to_slick_source.slick_source
+        ) AS slick_to_slick_source_slick_source
+        GROUP BY slick_to_slick_source_slick_source.slick) AS slick_source
+    ON slick_source.slick = slick.id;
+    """,
+    )
+    op.create_entity(slick_with_eez_and_source)
+
+    slick_with_source = PGView(
+        schema="public",
+        signature="slick_with_source",
+        definition="// not needed",
+    )
+    op.drop_entity(slick_with_source)
