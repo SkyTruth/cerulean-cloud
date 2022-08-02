3b3894ad5c7eb5108606cad80faae479b785cb90
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-08-02 12:46:50 +0200

    Add auth module

---
+++ cerulean_cloud/auth.py
@@ -0,0 +1,15 @@
+"""Generic auth module for fast API using hardcoded api key"""
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+api_keys = ["some_api_key"]  # This is encrypted in the database
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    """api key auth dependency"""
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
+        )
