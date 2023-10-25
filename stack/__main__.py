ba5984a908b6c24a0265740a1b9805045500c9d5
Leo Thomas <leo.thomas@mail.mcgill.ca>
2023-10-25 18:13:48 -0400

    Try disabling GCP cloud run service export values

--- stack/__main__.py
+++ stack/__main__.py
@@ -13,13 +13,13 @@ import titiler_sentinel as titiler_sentinel
 
 # Export the DNS name of the bucket
 pulumi.export("titiler_sentinel_url", titiler_sentinel.lambda_api.api_endpoint)
-pulumi.export(
-    "cloud_run_offset_tile_url", cloud_run_offset_tile.default.statuses[0].url
-)
-pulumi.export(
-    "cloud_run_orchestrator_url", cloud_run_orchestrator.default.statuses[0].url
-)
-pulumi.export("cloud_run_tipg_url", cloud_run_tipg.default.statuses[0].url)
+# pulumi.export(
+#    "cloud_run_offset_tile_url", cloud_run_offset_tile.default.statuses[0].url
+# )
+# pulumi.export(
+#    "cloud_run_orchestrator_url", cloud_run_orchestrator.default.statuses[0].url
+# )
+# pulumi.export("cloud_run_tipg_url", cloud_run_tipg.default.statuses[0].url)
 pulumi.export("database_url", database.sql_instance_url)
 pulumi.export("database_instance_name", database.instance.connection_name)
 pulumi.export("database_url_alembic", database.sql_instance_url_alembic)
