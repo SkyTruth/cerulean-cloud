90cdbe4295d448f99e9f92c4aa0f86eb2fd2ec37
Leo Thomas <leo.thomas@mail.mcgill.ca>
2023-10-25 18:49:46 -0400

    Reset stack outputs to original state

--- stack/__main__.py
+++ stack/__main__.py
@@ -3,10 +3,9 @@
 import cloud_function_ais_analysis
 import cloud_function_historical_run
 import cloud_function_scene_relevancy
-
-# import cloud_run_offset_tile
-# import cloud_run_orchestrator
-# import cloud_run_tipg
+import cloud_run_offset_tile
+import cloud_run_orchestrator
+import cloud_run_tipg
 import database
 import pulumi
 import sns_subscription
@@ -14,13 +13,13 @@ import titiler_sentinel as titiler_sentinel
 
 # Export the DNS name of the bucket
 pulumi.export("titiler_sentinel_url", titiler_sentinel.lambda_api.api_endpoint)
-# pulumi.export(
-#    "cloud_run_offset_tile_url", cloud_run_offset_tile.default.statuses[0].url
-# )
-# pulumi.export(
-#    "cloud_run_orchestrator_url", cloud_run_orchestrator.default.statuses[0].url
-# )
-# pulumi.export("cloud_run_tipg_url", cloud_run_tipg.default.statuses[0].url)
+pulumi.export(
+    "cloud_run_offset_tile_url", cloud_run_offset_tile.default.statuses[0].url
+)
+pulumi.export(
+    "cloud_run_orchestrator_url", cloud_run_orchestrator.default.statuses[0].url
+)
+pulumi.export("cloud_run_tipg_url", cloud_run_tipg.default.statuses[0].url)
 pulumi.export("database_url", database.sql_instance_url)
 pulumi.export("database_instance_name", database.instance.connection_name)
 pulumi.export("database_url_alembic", database.sql_instance_url_alembic)
