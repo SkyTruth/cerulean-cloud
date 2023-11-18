decd02a0485c0e9bb6ccfa33410dc8bc5e2d66fe
Leo Thomas <leo.thomas@mail.mcgill.ca>
2023-11-18 14:01:26 -0500

    Remove autogenerated columns (but leave columns), remove comments

--- alembic/versions/cb7ceecc3f87_add_funcs_on_insert.py
+++ alembic/versions/cb7ceecc3f87_add_funcs_on_insert.py
@@ -85,63 +85,6 @@ def upgrade() -> None:
         )
     )
 
-    # # Create a trigger function that maps slick to aoi upon insert
-    # op.execute(
-    #     """
-    #     CREATE OR REPLACE FUNCTION map_slick_to_aoi()
-    #     RETURNS TRIGGER AS $$
-    #     BEGIN
-    #         INSERT INTO slick_to_aoi (slick, aoi)
-    #         SELECT DISTINCT NEW.id, aoi.id FROM aoi
-    #         WHERE ST_Intersects(aoi.geometry, NEW.geometry);
-    #         RETURN NEW;
-    #     END;
-    #     $$ LANGUAGE plpgsql;
-    #     """
-    # )
-
-    # # Create a trigger that calls the trigger function after insert
-    # op.execute(
-    #     """
-    #     CREATE TRIGGER trigger_map_slick_to_aoi
-    #     AFTER INSERT ON slick
-    #     FOR EACH ROW
-    #     EXECUTE FUNCTION map_slick_to_aoi();
-    #     """
-    # )
-
-    # op.execute(
-    #     """
-    #     CREATE OR REPLACE FUNCTION populate_cls()
-    #     RETURNS TRIGGER AS
-    #     $$
-    #     BEGIN
-    #         NEW.cls := (
-    #             SELECT cls.id
-    #             FROM cls
-    #             JOIN orchestrator_run ON NEW.orchestrator_run = orchestrator_run.id
-    #             JOIN LATERAL json_each_text((SELECT cls_map FROM model WHERE id = orchestrator_run.model))
-    #                 m(key, value)
-    #                 ON key::integer = NEW.inference_idx
-    #                 WHERE cls.short_name = value
-    #             LIMIT 1
-    #         );
-    #         RETURN NEW;
-    #     END;
-    #     $$
-    #     LANGUAGE plpgsql;
-    #     """
-    # )
-
-    # op.execute(
-    #     """
-    #     CREATE TRIGGER trigger_populate_cls
-    #     BEFORE INSERT ON slick
-    #     FOR EACH ROW
-    #     EXECUTE FUNCTION populate_cls();
-    #     """
-    # )
-
 
 def downgrade() -> None:
     """Add funcs on insert"""
@@ -155,24 +98,3 @@ def downgrade() -> None:
         DROP FUNCTION IF EXISTS slick_before_trigger_func();
         """
     )
-
-    # op.execute(
-    #     """
-    #     DROP TRIGGER IF EXISTS trigger_map_slick_to_aoi ON slick;
-    #     """
-    # )
-    # op.execute(
-    #     """
-    #     DROP FUNCTION IF EXISTS map_slick_to_aoi();
-    #     """
-    # )
-    # op.execute(
-    #     """
-    #     DROP TRIGGER IF EXISTS trigger_populate_cls ON slick;
-    #     """
-    # )
-    # op.execute(
-    #     """
-    #     DROP FUNCTION IF EXISTS populate_cls();
-    #     """
-    # )
