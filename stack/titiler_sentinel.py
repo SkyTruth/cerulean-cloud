b6fb71e0686b673929ded18731daf410435c4d74
Jona <jona.raphael@gmail.com>
2023-11-27 13:06:40 -0500

    attempting to populate the AWS pulumi resources with a secret stored in GCP

--- stack/titiler_sentinel.py
+++ stack/titiler_sentinel.py
@@ -8,7 +8,7 @@ titiler_keyname = pulumi.Config("cerulean-cloud").require("titiler_keyname")
 secret = gcp.secretmanager.get_secret(secret_id=titiler_keyname)
 titiler_api_key = gcp.secretmanager.get_secret_version_output(
     secret=titiler_keyname
-).secret_data.apply(lambda data: data.decode("utf-8"))
+).secret_data
 
 
 s3_bucket = aws.s3.Bucket(construct_name("titiler-lambda-archive"))
