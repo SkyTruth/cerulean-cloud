6afa41a3fd7f966cb877393a8b62c405aebf49c0
Jona <jona.raphael@gmail.com>
2024-04-25 13:04:42 -0400

    Oops!

--- stack/database.py
+++ stack/database.py
@@ -65,7 +65,7 @@ users = gcp.sql.User(
     name=db_name,
     instance=instance.name,
     password=pulumi.Config("db").require_secret("db-password"),
-    opts=pulumi.ResourceOptions(dependsOn=[database]),
+    opts=pulumi.ResourceOptions(depends_on=[database]),
 )
 
 sql_instance_url_with_asyncpg = pulumi.Output.concat(
