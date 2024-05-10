00165465e523fa7e7334c0390346e2fdc4323c87
Jona <jona.raphael@gmail.com>
2024-05-10 14:55:10 -0400

    Try importing merge separately

--- cerulean_cloud/models.py
+++ cerulean_cloud/models.py
@@ -16,6 +16,7 @@ import torch
 import torchvision  # noqa
 from rasterio.features import shapes
 from rasterio.io import MemoryFile
+from rasterio.merge import rio_merge
 from scipy.ndimage import label
 from shapely.geometry import MultiPolygon, shape
 
@@ -297,7 +298,7 @@ class FASTAIUNETModel(BaseModel):
             ]
 
             logging.info("Merging tiles!")
-            scene_array, transform = rasterio.merge.merge(ds_tiles)
+            scene_array, transform = rio_merge(ds_tiles)
             return scene_array, transform
         finally:
             for ds in ds_tiles:
