329bee58129dd2ca4eb4084a6fb28de950dfc7f7
Anthony Lukach <anthonylukach@gmail.com>
2024-07-31 17:00:32 -0700

    Make filename stable across deployments

--- stack/cloud_function_historical_run.py
+++ stack/cloud_function_historical_run.py
@@ -1,7 +1,5 @@
 """cloud function to select appropriate scenes (over water and IW) from SNS notification"""
 
-import time
-
 import cloud_function_scene_relevancy
 import cloud_run_orchestrator
 import database
@@ -37,7 +35,7 @@ archive = package.apply(lambda x: pulumi.FileAsset(x))
 # source code. ("main.py" and "requirements.txt".)
 source_archive_object = storage.BucketObject(
     construct_name("source-cf-historical-run"),
-    name=f"handler.py-{time.time():f}",
+    name="handler.py",
     bucket=cloud_function_scene_relevancy.bucket.name,
     source=archive,
 )
