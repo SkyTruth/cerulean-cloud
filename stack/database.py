6ff11923da13f87b8405d5a2a893e8cad0126d40
Jona <jona.raphael@gmail.com>
2024-09-17 13:56:51 -0400

    Try different

--- stack/database.py
+++ stack/database.py
@@ -74,7 +74,7 @@ sql_instance_url_with_asyncpg = pulumi.Output.concat(
     ":",
     pulumi.Config("db").require_secret("db-password"),
     "@",
-    instance.connection_name,
+    instance.first_ip_address,
     "/",
     db_name,
 )
@@ -84,7 +84,7 @@ sql_instance_url = pulumi.Output.concat(
     ":",
     pulumi.Config("db").require_secret("db-password"),
     "@",
-    instance.connection_name,
+    instance.first_ip_address,
     "/",
     db_name,
 )
