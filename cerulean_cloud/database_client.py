6b637bbbfd9ebadfc12c7883016a660b770d741d
Jona <jona.raphael@gmail.com>
2024-11-22 00:37:50 -0500

    Cleaner true expression

--- cerulean_cloud/database_client.py
+++ cerulean_cloud/database_client.py
@@ -335,7 +335,7 @@ class DatabaseClient:
                     .where(
                         and_(
                             db.Sentinel1Grd.scene_id == scene_id,
-                            db.Slick.active.is_(True),
+                            db.Slick.active,
                         )
                     )
                 )
@@ -367,7 +367,7 @@ class DatabaseClient:
                     .where(
                         and_(
                             db.SlickToSource.slick == slick_id,
-                            db.SlickToSource.active.is_(True),
+                            db.SlickToSource.active,
                         )
                     )
                 )
