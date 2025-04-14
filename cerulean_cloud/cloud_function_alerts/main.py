a2d9ed8c33a345e59d93c257251057ca403a521c
sstill88 <38501385+sstill88@users.noreply.github.com>
2025-04-14 12:09:19 -0700

    Feat/alerts (#149)
    * skeleton
    
    * added secret webhook, check cerulean API for new slicks
    
    * docstrings and add alerts testing
    
    * get webhook secret ID from environment variable, add secrets access
    
    * update requirements file
    
    * renamed handler-->main, cloud v1->v2, package via assetarchive, not creating bucket object
    
    * updated __main__.py and matched formatting
    
    * typo...
    
    * invoker: region --> location, v1 --> v2
    
    * v1 --> v2 params..ugh..
    
    * source in function params, not build_config...?
    
    * add bucket and archive, make that the source for cfv2
    
    * M --> Mi. arghhh
    
    * add trigger argument to the handler
    
    * full path_to_source
    
    * change archive format
    
    * set up to accept http traffic as input (I believe this is needed to be run via scheduler)
    
    * fix service account id in stack
    
    * ./ --> ../ ugh
    
    * create zip and build asset archive from that in stack code
    
    * create oidc token for http target
    
    * typo...
    
    * update invoker 'member' access
    
    * string-ify the service account email...
    
    * failed to authenticate debugging...
    
    * open invoker to allUsers
    
    * debugging...
    
    * whoops, alerts for wrong reason
    
    * force rebuild
    
    * more debugging...
    
    * add back service account permissions, debug
    
    * more debugging...
    
    * why isn't it working anymore :(
    
    * schedule for 8 a.m.
    
    * add cloudrun IamMember
    
    * rename resource to rebuild
    
    * update tz --> utc, revert resource, set to 8 a.m. alert
    
    * add connection retries and failure message
    
    * docstrings
    
    * update requirements
    
    * update allowed traffic
    
    * update source object to randomize name
    
    * change how secrets are handled
    
    * update replace_on_changes opts, test that secrets are being accessed
    
    * remove a replace_on_changes (not recognized) and removed secrets testing (success)
    
    * check for slicks of different types
    
    * get tipg url from environment variable
    
    * add TIPG_URL to slack alert
    
    * updated alerts keyname in Pulumi yaml
    
    * added dry run option
    
    * cleanup
    
    * added MINIMUM_RESULT_COUNT
    
    * testing prod environ
    
    * more testing
    
    * finalize
    
    ---------
    
    Co-authored-by: Jona <jona.raphael@gmail.com>

---
+++ cerulean_cloud/cloud_function_alerts/main.py
@@ -0,0 +1,101 @@
+from flask import Request, make_response
+import datetime
+import os
+import requests
+import time
+
+WEBHOOK_URL = os.environ["SLACK_ALERTS_WEBHOOK"]
+TIPG_URL = os.environ["TIPG_URL"]
+DRY_RUN = os.getenv("IS_DRY_RUN", "").lower() == "true"
+
+BASE_URL = f"{TIPG_URL}/collections/public.source_plus"
+MAX_RETRIES = 3
+RETRY_DELAY_SECONDS = 2
+MINIMUM_RESULT_COUNT = 1
+SLICK_TYPES = ["VESSEL", "INFRA"]  # , "DARK", "NATURAL"]
+
+
+def send_alert_no_recent_slicks_message(slick_type, dry_run=DRY_RUN):
+    """
+    Sends `no slick` warning to Slack channel
+    """
+    if dry_run:
+        print(f"No new {slick_type} slicks in the last 24 hours in {TIPG_URL}")
+    else:
+        _ = requests.post(
+            WEBHOOK_URL,
+            json=f"No new {slick_type} slicks in the last 24 hours in {TIPG_URL}",
+        )
+
+
+def send_alert_failed_connection_message(url, dry_run=DRY_RUN):
+    """
+    Sends `no slick` warning to Slack channel
+    """
+    if dry_run:
+        print(f"Failed connection to {url}")
+    else:
+        _ = requests.post(WEBHOOK_URL, json={"text": f"Failed connection to {url}"})
+
+
+def send_success_message(slick_type, num_matched):
+    """
+    Perform success function (generally used for testing)
+    """
+    print(f"No errors detected; found {num_matched} {slick_type} slicks")
+
+
+def fetch_with_retries(st, fn, base_url=BASE_URL, dry_run=DRY_RUN, slick_type=None):
+    """
+    Attempts to fetch data from the slick detection API with retry logic.
+
+    If data is found (`numberMatched > 0`), a success message is sent.
+    If no data is found, a "no slicks" alert is triggered.
+    On repeated connection failures, a failure alert is sent.
+
+    Parameters:
+        st (str): Start datetime (ISO format).
+        fn (str): End datetime (ISO format).
+        slick_type (str, optional): Optional source type to filter the query.
+    """
+    src_var = "" if slick_type is None else f"&source_type={slick_type}"
+    url = f"{base_url}/items?limit=1{src_var}&datetime={st}/{fn}"
+    for attempt in range(1, MAX_RETRIES + 1):
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            num_matched = response.json()["numberMatched"]
+
+            if num_matched > MINIMUM_RESULT_COUNT:
+                send_success_message(slick_type, num_matched)
+            else:
+                send_alert_no_recent_slicks_message(slick_type, dry_run=dry_run)
+            return
+
+        except requests.exceptions.RequestException:
+            if attempt < MAX_RETRIES:
+                time.sleep(RETRY_DELAY_SECONDS)
+            else:
+                send_alert_failed_connection_message(url, dry_run=dry_run)
+
+
+def check_recent_slicks(dry_run=DRY_RUN):
+    """
+    Hits the Cerulean API to see if any new slicks have been added in the past 24 hours
+    """
+
+    end_date = datetime.datetime.now(datetime.timezone.utc)
+    start_date = end_date - datetime.timedelta(days=1)
+    st = start_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    fn = end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    for slick_type in SLICK_TYPES:
+        fetch_with_retries(
+            st, fn, base_url=BASE_URL, dry_run=dry_run, slick_type=slick_type
+        )
+
+
+def main(request: Request):
+    print(f"Base URL: {BASE_URL}; dry run: {os.getenv('IS_DRY_RUN', '').lower()}")
+    check_recent_slicks(dry_run=DRY_RUN)
+    return make_response("Function executed", 200)
---
+++ stack/cloud_function_alerts.py
@@ -0,0 +1,117 @@
+import time
+
+import cloud_run_tipg
+import pulumi
+import pulumi_gcp as gcp
+from utils import construct_name, pulumi_create_zip
+
+PATH_TO_SOURCE_CODE = "../cerulean_cloud/cloud_function_alerts"
+resource_name = "cf-alerts"
+
+stack = pulumi.get_stack()
+
+# Create service account for this stack
+service_account = gcp.serviceaccount.Account(
+    construct_name(f"{resource_name}-service-account"),
+    account_id=f"{stack}-{resource_name}",
+    display_name="Service account for Slack alert function",
+)
+
+# Create bucket to store handler
+bucket = gcp.storage.Bucket(
+    construct_name(f"{resource_name}-bucket"),
+    location=pulumi.Config("gcp").require("region"),
+    labels={"pulumi": "true", "environment": stack},
+)
+
+# Create and zip package to be basis of CloudFunction
+package = pulumi_create_zip(
+    dir_to_zip=PATH_TO_SOURCE_CODE,
+    zip_filepath="../cloud_function_alerts.zip",
+)
+archive = package.apply(lambda x: pulumi.FileAsset(x))
+
+# Create the Cloud Storage object containing the zipped CloudFunction
+source_archive_object = gcp.storage.BucketObject(
+    construct_name(f"{resource_name}-source"),
+    name=construct_name(f"{resource_name}-source-{time.time():f}"),
+    bucket=bucket.name,
+    source=archive,
+)
+
+# Define secret environment variables
+slack_webhooks = {
+    "key": "SLACK_ALERTS_WEBHOOK",
+    "secret": pulumi.Config("alerts").require("keyname"),
+    "version": "latest",
+    "project_id": pulumi.Config("gcp").require("project"),
+}
+
+# Create the CloudFunction (v2)
+fxn = gcp.cloudfunctionsv2.Function(
+    construct_name(resource_name),
+    name=construct_name(resource_name),
+    location=pulumi.Config("gcp").require("region"),
+    description="Cloud Function for Pipeline Failure Alerting",
+    build_config=gcp.cloudfunctionsv2.FunctionBuildConfigArgs(
+        runtime="python311",
+        entry_point="main",
+        source=gcp.cloudfunctionsv2.FunctionBuildConfigSourceArgs(
+            storage_source=gcp.cloudfunctionsv2.FunctionBuildConfigSourceStorageSourceArgs(
+                bucket=bucket.name,
+                object=source_archive_object.name,
+            ),
+        ),
+    ),
+    service_config=gcp.cloudfunctionsv2.FunctionServiceConfigArgs(
+        max_instance_count=1,
+        available_memory="128Mi",
+        timeout_seconds=60,
+        ingress_settings="ALLOW_INTERNAL_ONLY",
+        environment_variables={
+            "GCP_PROJECT": gcp.config.project,
+            "TIPG_URL": cloud_run_tipg.default.statuses[0].url,
+            "IS_DRY_RUN": pulumi.Config("cerulean-cloud").require("dryrun_alerts"),
+        },
+        secret_environment_variables=[slack_webhooks],
+    ),
+)
+
+# IAM entry for all users to invoke the function
+invoker = gcp.cloudfunctionsv2.FunctionIamMember(
+    construct_name(f"{resource_name}-invoker"),
+    project=fxn.project,
+    location=fxn.location,
+    cloud_function=fxn.name,
+    role="roles/cloudfunctions.invoker",
+    member=pulumi.Output.concat("serviceAccount:", service_account.email),
+    opts=pulumi.ResourceOptions(replace_on_changes=["member"]),
+)
+
+cloud_run_invoker = gcp.cloudrun.IamMember(
+    construct_name(f"{resource_name}-cr-invoker"),
+    service=fxn.name,
+    location=fxn.location,
+    role="roles/run.invoker",
+    member=pulumi.Output.concat("serviceAccount:", service_account.email),
+    opts=pulumi.ResourceOptions(replace_on_changes=["member"]),
+)
+
+
+http_target = gcp.cloudscheduler.JobHttpTargetArgs(
+    http_method="GET",
+    uri=fxn.service_config.uri,
+    oidc_token=gcp.cloudscheduler.JobHttpTargetOidcTokenArgs(
+        service_account_email=service_account.email,
+    ),
+)
+
+
+job = gcp.cloudscheduler.Job(
+    construct_name(f"{resource_name}-scheduler"),
+    description="Run test daily",
+    schedule="0 8 * * *",  # 8 AM
+    time_zone="America/New_York",
+    http_target=http_target,
+    opts=pulumi.ResourceOptions(replace_on_changes=["schedule"]),
+)
