e215e2b125ffd7a1bced420c1fb05ff07f0dcfe4
Jona <jona.raphael@gmail.com>
2025-06-25 16:03:01 -0400

    Update source URL

--- alembic/versions/39277f6278f4_add_view.py
+++ alembic/versions/39277f6278f4_add_view.py
@@ -79,7 +79,11 @@ def upgrade() -> None:
                 sts.create_time as create_time,
                 sts.git_hash as git_tag,
                 'https://cerulean.skytruth.org/slicks/' || sk.id::text ||'?ref=api&slick_id=' || sk.id AS slick_url,
-                'https://cerulean.skytruth.org/?ref=api&source_score=0_Infinity&' || st.ext_id_name || '=' || s.ext_id AS source_url
+                CASE
+                    WHEN s.type = 1 THEN ('https://cerulean.skytruth.org/sources/'::text || s.ext_id) || '?ref=api'::text
+                    WHEN s.type = 2 THEN (('https://cerulean.skytruth.org/?ref=api&source_score=0_Infinity&'::text || st.ext_id_name) || '='::text) || s.ext_id
+                    ELSE NULL::text
+                END AS source_url
             FROM
                 slick_to_source sts
             INNER JOIN
