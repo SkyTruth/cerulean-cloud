ea18bd8d0b9103097084a924b1dc4baf75b6ca10
Jona <jona.raphael@gmail.com>
2026-04-23 15:18:03 -0400

    handled by CONSTRUCTIVE branch

--- alembic/versions/4d8b6f4e6d2a_drop_legacy_aoi_geometry_storage.py
+++ alembic/versions/4d8b6f4e6d2a_drop_legacy_aoi_geometry_storage.py
@@ -18,143 +18,6 @@ down_revision = "1f70e7d0c5b1"
 branch_labels = None
 depends_on = None
 
-SLICK_TRIGGER_FUNC_WITH_AOI_CHUNKS = """
-    CREATE OR REPLACE FUNCTION slick_before_trigger_func()
-    RETURNS trigger
-    AS $$
-    DECLARE
-        timer timestamptz := clock_timestamp();
-        _geog geography := NEW.geometry;
-        _geom geometry;
-        oriented_envelope geometry;
-        oe_ring geometry;
-        rec record;
-    BEGIN
-        RAISE NOTICE '---------------------------------------------------------';
-        RAISE NOTICE 'In slick_before_trigger_func. %', (clock_timestamp() - timer)::interval;
-        _geom := _geog::geometry;
-        oriented_envelope := st_orientedenvelope(_geom);
-        oe_ring := st_exteriorring(oriented_envelope);
-        NEW.geometry_count := st_numgeometries(_geom);
-        NEW.largest_area := (
-            SELECT MAX(st_area((poly.geom)::geography))
-            FROM st_dump(_geom) AS poly
-        );
-        NEW.median_area := (
-            SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY st_area((poly.geom)::geography))
-            FROM st_dump(_geom) AS poly
-        );
-        NEW.area := st_area(_geog);
-        NEW.centroid := st_centroid(_geog);
-        NEW.perimeter = st_perimeter(_geog);
-        NEW.polsby_popper := 4.0 * pi() * NEW.area / (NEW.perimeter ^ 2.0);
-        NEW.fill_factor := NEW.area / st_area(oriented_envelope::geography);
-        NEW.length := GREATEST(
-            st_distance(
-                st_pointn(oe_ring,1)::geography,
-                st_pointn(oe_ring,2)::geography
-            ),
-            st_distance(
-                st_pointn(oe_ring,2)::geography,
-                st_pointn(oe_ring,3)::geography
-            )
-        );
-        RAISE NOTICE 'Calculated all generated fields. %', (clock_timestamp() - timer)::interval;
-        NEW.cls := COALESCE(
-            NEW.cls,
-            (
-                SELECT cls.id
-                FROM cls
-                JOIN orchestrator_run ON NEW.orchestrator_run = orchestrator_run.id
-                JOIN LATERAL json_each_text((SELECT cls_map FROM model WHERE id = orchestrator_run.model))
-                    m(key, value)
-                    ON key::integer = NEW.inference_idx
-                WHERE cls.short_name = CASE
-                    WHEN value = 'BACKGROUND' THEN 'NOT_OIL'
-                    ELSE value
-                END
-                LIMIT 1
-            )
-        );
-        RAISE NOTICE 'Calculated NEW.cls. %', (clock_timestamp() - timer)::interval;
-
-        INSERT INTO slick_to_aoi(slick, aoi)
-        SELECT DISTINCT NEW.id, aoi_chunks.id
-        FROM aoi_chunks
-        WHERE st_intersects(_geom, aoi_chunks.geometry);
-
-        RAISE NOTICE 'Insert done to slick_to_aoi. %', (clock_timestamp() - timer)::interval;
-
-        RETURN NEW;
-    END;
-    $$ LANGUAGE PLPGSQL;
-"""
-
-SLICK_TRIGGER_FUNC_WITHOUT_AOI_CHUNKS = """
-    CREATE OR REPLACE FUNCTION slick_before_trigger_func()
-    RETURNS trigger
-    AS $$
-    DECLARE
-        timer timestamptz := clock_timestamp();
-        _geog geography := NEW.geometry;
-        _geom geometry;
-        oriented_envelope geometry;
-        oe_ring geometry;
-        rec record;
-    BEGIN
-        RAISE NOTICE '---------------------------------------------------------';
-        RAISE NOTICE 'In slick_before_trigger_func. %', (clock_timestamp() - timer)::interval;
-        _geom := _geog::geometry;
-        oriented_envelope := st_orientedenvelope(_geom);
-        oe_ring := st_exteriorring(oriented_envelope);
-        NEW.geometry_count := st_numgeometries(_geom);
-        NEW.largest_area := (
-            SELECT MAX(st_area((poly.geom)::geography))
-            FROM st_dump(_geom) AS poly
-        );
-        NEW.median_area := (
-            SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY st_area((poly.geom)::geography))
-            FROM st_dump(_geom) AS poly
-        );
-        NEW.area := st_area(_geog);
-        NEW.centroid := st_centroid(_geog);
-        NEW.perimeter = st_perimeter(_geog);
-        NEW.polsby_popper := 4.0 * pi() * NEW.area / (NEW.perimeter ^ 2.0);
-        NEW.fill_factor := NEW.area / st_area(oriented_envelope::geography);
-        NEW.length := GREATEST(
-            st_distance(
-                st_pointn(oe_ring,1)::geography,
-                st_pointn(oe_ring,2)::geography
-            ),
-            st_distance(
-                st_pointn(oe_ring,2)::geography,
-                st_pointn(oe_ring,3)::geography
-            )
-        );
-        RAISE NOTICE 'Calculated all generated fields. %', (clock_timestamp() - timer)::interval;
-        NEW.cls := COALESCE(
-            NEW.cls,
-            (
-                SELECT cls.id
-                FROM cls
-                JOIN orchestrator_run ON NEW.orchestrator_run = orchestrator_run.id
-                JOIN LATERAL json_each_text((SELECT cls_map FROM model WHERE id = orchestrator_run.model))
-                    m(key, value)
-                    ON key::integer = NEW.inference_idx
-                WHERE cls.short_name = CASE
-                    WHEN value = 'BACKGROUND' THEN 'NOT_OIL'
-                    ELSE value
-                END
-                LIMIT 1
-            )
-        );
-        RAISE NOTICE 'Calculated NEW.cls. %', (clock_timestamp() - timer)::interval;
-
-        RETURN NEW;
-    END;
-    $$ LANGUAGE PLPGSQL;
-"""
-
 
 def upgrade() -> None:
     """Drop legacy AOI geometry storage."""
@@ -187,7 +50,6 @@ def upgrade() -> None:
     # transaction before dropping AOI tables.
     op.execute("SET CONSTRAINTS ALL IMMEDIATE")
     op.execute("DROP RULE IF EXISTS bypass_slick_to_aoi_insert ON public.slick_to_aoi")
-    op.execute(SLICK_TRIGGER_FUNC_WITHOUT_AOI_CHUNKS)
 
     op.drop_column("orchestrator_run", "sea_ice_date")
     op.drop_table("aoi_chunks")
@@ -264,7 +126,6 @@ def downgrade() -> None:
         """
     )
 
-    op.execute(SLICK_TRIGGER_FUNC_WITH_AOI_CHUNKS)
     op.execute(
         """
         CREATE OR REPLACE RULE bypass_slick_to_aoi_insert
